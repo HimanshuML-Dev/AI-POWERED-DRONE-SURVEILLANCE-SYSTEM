05ecf66c5577798207f62fd6f701b9a7469a0ee5
Himanshu Nagar <himanshunagar807@gmail.com>
2026-09-13 18:17:11 +0530

    new file:   detection/classes.py 	new file:   detection/detector.py

---
+++ detection/classes.py
@@ -0,0 +1,12 @@
+COCO_CLASSES = {
+    0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 5: 'bus', 7: 'truck'
+}
+
+CLASS_COLOR_MAP = {
+    'person': (255, 56, 56),      # Red
+    'car': (56, 255, 255),        # Yellow
+    'motorcycle': (255, 157, 151),# Pink
+    'bus': (31, 112, 255),        # Blue
+    'truck': (29, 178, 255),      # Cyan
+    'bicycle': (114, 114, 255)    # Light Blue
+}
